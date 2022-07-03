da82f26ea7aa8c90fff5f880b1ea23981e9a67bf
4goUnited <4goUnited@gmail.com>
2022-07-03 11:56:30 +0500

    Added all needed func

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +10,37 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+func handleName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Hello, %v!", vars["PARAM"])
+}
+
+func handleBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Can't read body from POST request: %w", err))
+	}
+	fmt.Fprintf(w, "I got message:\n%v", string(data))
+}
+
+func handleHeaders(w http.ResponseWriter, r *http.Request) {
+	a := r.Header.Get("a")
+	b := r.Header.Get("b")
+	Aa, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Can't parse header 'a' to int : %w", err))
+	}
+	Ab, err := strconv.Atoi(b)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Can't parse header 'b' to int : %w", err))
+	}
+	w.Header().Set("a+b", strconv.Itoa(Aa + Ab))
+}
 
 /**
 Please note Start functions is a placeholder for you to start your own solution.
@@ -20,6 +52,10 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", handleName).Methods("GET")
+	router.HandleFunc("/bad", handleBad).Methods("GET")
+	router.HandleFunc("/data", handleData).Methods("POST")
+	router.HandleFunc("/headers", handleHeaders).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
